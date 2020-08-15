c44f4ef939bd909b95d9d1702c2a6dceca0b3cab
diffen <diffen@me.com>
2020-08-15 10:42:30 +0200

    support for login

--- diffen.py
+++ diffen.py
@@ -17,6 +17,51 @@ def discord():
 def weight_tracker():
 	return render_template('weight_tracker.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You are now logged out!")
+    return redirect(url_for('index'))
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+
+            flash('Logged in successfully!')
+
+            next = request.args.get('next')
+
+            if next == None or not next[0]=='/':
+                next = url_for('welcome_user')
+
+            return redirect(next)
+
+    return render_template('login.html',form=form)
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+            name=form.username.data,
+            password=form.password.data)
+        
+        db.session.add(user)
+        db.session.commit()
+        flash("You have successfully registered!")
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html',form=form)
+
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
 
